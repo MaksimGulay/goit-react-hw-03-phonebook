2df2ff8835ae62bb55dac6ea9661a7e185b2ef99
Maksim Gulay <maksimgulaj@gmail.com>
2023-08-22 13:38:24 +0300

    add methods - "Component Lifecycle"

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      this.setState({
+        contacts: JSON.parse(savedContacts),
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   addNewContact = newContact => {
     const { contacts } = this.state;
     const existingContact = contacts.find(
